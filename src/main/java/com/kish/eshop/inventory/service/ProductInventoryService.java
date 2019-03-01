8a3172a620aacc4d148f301c0157587b93f830ee
wodehai_glb <goodMorning_glb@163.com>
2019-03-01 15:33:00 +0800

    first commit

---
+++ src/main/java/com/kish/eshop/inventory/service/ProductInventoryService.java
@@ -0,0 +1,15 @@
+package com.kish.eshop.inventory.service;
+
+import com.kish.eshop.inventory.model.ProductInventory;
+
+public interface ProductInventoryService {
+	
+	public void add(ProductInventory productInventory);
+	
+	public void update(ProductInventory productInventory);
+	
+	public void delete(Long id);
+	
+	public ProductInventory findById(Long id);
+	
+}
---
+++ src/test/java/com/kish/eshop/inventory/AppTest.java
@@ -0,0 +1,20 @@
+package com.kish.eshop.inventory;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
